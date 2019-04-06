f05d45d882d98be2420097892aa7d801c3c48322
unknown <kentjl287@gmail.com>
2019-04-06 17:02:19 +0800

    paypal btn

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PaypalBtn from './PaypalBtn';
 
-export default function CartTotals({value}) {
+export default function CartTotals({value, history}) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
 
   return (
@@ -15,6 +16,7 @@ export default function CartTotals({value}) {
                     <h5><span className="text-title">subTotal :</span><strong>$ {cartSubTotal}</strong></h5>
                     <h5><span className="text-title">Tax :</span><strong>$ {cartTax}</strong></h5>
                     <h5><span className="text-title">Total :</span><strong>$ {cartTotal}</strong></h5>
+                    <PaypalBtn total={cartTotal} clearCart={clearCart} history={history}/>
                 </div>
             </div>
         </div>
